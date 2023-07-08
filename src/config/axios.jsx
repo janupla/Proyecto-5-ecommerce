982e9e885ccdef2df0579bf91f73eb88973dcff3
Jana <janagonzalez.f@gmail.com>
2023-07-07 20:47:05 -0400

    configurando axios

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const backendUrlLocal = "http://localhost:4500"
-const backendUrlProd = "https://api-express-6zho.onrender.com"
-const axiosClient = axios.create({
-    baseURL: backendUrlLocal
-})
+const backendUrlLocal = "http://localhost:4500" // define url backend
+const backendUrlProd = "" //esta url es la de producción que está en RENDER
+const axiosClient = axios.create({ //llama librería axios, método create
+    baseURL: backendUrlLocal //cuando pruebo local, dejo la urlLocal, pero si pruebo como producción en render 
+})                            //debo poner en baseURL: backendUrlProd
 
 export default axiosClient
