d806cdd04d98cb80598eb29b2d1b35e3fb46ca92
Jana <janagonzalez.f@gmail.com>
2023-07-11 00:01:02 -0400

    instalando paypal

--- src/components/shoppingCart/ShoppingCart.jsx
+++ src/components/shoppingCart/ShoppingCart.jsx
@@ -4,30 +4,32 @@ import { useNavigate } from 'react-router-dom'
 import ShoppingCartContext from '../../contexts/ShoppingCart/ShoppingCartContext.jsx'
 
 export const ShoppingCart = ({ showShoppingCart, handleCloseShoppingCart }) => {
-    const shoppingCartCtx = useContext(ShoppingCartContext)
-    const { products, getProducts, removeProduct } = shoppingCartCtx
-    const [total, setTotal] = useState(0)
+    const shoppingCartCtx = useContext(ShoppingCartContext) //contexto carrito, baúl contenedor
+    const { products, getProducts, removeProduct } = shoppingCartCtx //extrae listado de productos, obtine y remueve
+    const [total, setTotal] = useState(0) //esatdo total comienza ne cero
     const navigate = useNavigate()
-    useEffect(() => {
+    useEffect(() => {          //ve si está definido o no.
         if (!products) {
             getProducts()
-        } else {
+        } else {         // si está definido setea a cero  y x cada producto extrae precio y cantidad
             setTotal(0)
             products.forEach(product => {
                 const { price, quantity } = product
-                setTotal((current) => {
+                setTotal((current) => {            // setea el nuevo total toma valor actual y suma el precio x cantidad.
                     return current + price * quantity
                 })
             });
         }
     }, [products])
 
+    // modal ventana q se oculta y aparece, bootstrap
+
     return (
         <Modal show={showShoppingCart} onHide={handleCloseShoppingCart}>
             <Modal.Header closeButton>
                 <Modal.Title>Carrito de compras</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body> 
                 {
                     products?.length === 0 ? "No hay productos en el carrito"
                         : (
@@ -36,7 +38,7 @@ export const ShoppingCart = ({ showShoppingCart, handleCloseShoppingCart }) => {
                                     <tr>
                                         <th>#</th>
                                         <th>Nombre</th>
-                                        <th>Precio Unitario (USD)</th>
+                                        <th>Precio Unitario (USD)</th> 
                                         <th>Cantidad</th>
                                     </tr>
                                 </thead>
@@ -56,7 +58,7 @@ export const ShoppingCart = ({ showShoppingCart, handleCloseShoppingCart }) => {
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
                                                                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                                                             </svg>
-                                                        </Button>
+                                                        </Button> 
                                                     </td>
                                                 </tr>
                                             )
@@ -65,7 +67,7 @@ export const ShoppingCart = ({ showShoppingCart, handleCloseShoppingCart }) => {
                                 </tbody>
                                 <tfoot>
                                     <tr >
-                                        <td>Total a pagar: ${total} USD</td>
+                                        <td>Total a pagar: ${total} USD</td> 
                                     </tr>
                                 </tfoot>
                             </Table>
@@ -83,3 +85,4 @@ export const ShoppingCart = ({ showShoppingCart, handleCloseShoppingCart }) => {
         </Modal>
     )
 }
+//como usamos paypal debemos cobrar en dolares
